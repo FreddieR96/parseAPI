46ea0e8d4f603f57b8b8d230e740004630fe91ca
Freddie Rigby <rigbyfreddie685@gmail.com>
2019-08-19 00:27:58 +0100

    .

---
+++ parseapi.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+
+url = 'https://api.nhs.uk/search/?query=richmond+dentist'
+
+response = HTTParty.get(
+	url,
+	headers: { "subscription-key": [ENTER_SUBSCRIPTION_KEY }
+	)
+
+response.parsed_response
+puts response
+response["results"].each do |f|
+puts f["title"]
+end
